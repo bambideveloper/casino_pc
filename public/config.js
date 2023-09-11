de3588188da4b5a92a8d894de106bffb89c314f1
sniper19891112 <sniper62899@gmail.com>
2023-09-11 16:06:48 +0800

    updated

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 const configStatic={};
 
+configStatic.VITE_BASE_URL = "https://api.fg1698.com/api";
+configStatic.VITE_HOST_URL = "https://www.fg1698.com"
+configStatic.VITE_SOCKET_URL = "https://socket.fg1698.com"
+configStatic.VITE_FILE_BASE_URL = "https://api.fg1698.com/"
+configStatic.VITE_MOBILE_URL="https://www.fg1698a.com"
+
 configStatic.VITE_SERVICE_URL = "https://wap.hg909c.com"
 configStatic.VITE_LY_PAY_URL = "http://api2.spellsbet.com/v1/cash"
-configStatic.VITE_BASE_URL = "https://api.fg1698.com/api";
-configStatic.VITE_HOST_URL = "https://pc.pgdemo123.com";
-configStatic.VITE_SOCKET_URL = "https://socket.pgdemo123.com"
-configStatic.VITE_FILE_BASE_URL = " https://api.pgdemo123.com"
-configStatic.VITE_APP_URL = "https://www.pgdemo123.com"
-configStatic.VITE_MOBILE_URL="https://pgdemo123.com"
