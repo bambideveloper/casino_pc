32537e6aead6e1a5de07be7f8756d8f06b8b7c20
sniper19891112 <sniper62899@gmail.com>
2023-10-03 13:09:06 +0200

    updated

--- src/views/deposit_help/index.vue
+++ src/views/deposit_help/index.vue
@@ -1,43 +1,23 @@
-<script setup>
-import { ref } from 'vue';
-
-const itemList = ref([
-    {
-        img: new URL('@/assets/images/account/deposit-1.png', import.meta.url).href,
-    },
-    {
-        img: new URL('@/assets/images/account/deposit-2.png', import.meta.url).href,
-    },
-    {
-        img: new URL('@/assets/images/account/deposit-3.png', import.meta.url).href,
-    },
-    {
-        img: new URL('@/assets/images/account/deposit-4.png', import.meta.url).href,
-    }
-]);
-</script>
+<script setup></script>
 
 <template>
-    <div class="animated fadeInLeft pb-[20px] mb-[30px]">
-        <div class=" pt-[50px] pb-[60px] h-screen">
-            <div class="flex text-[15px] px-2 items-center py-3">
-                <div class="flex">
-                    <img class="w-[30px] h-[30px]" referrerpolicy="no-referrer"
-                        src="@/assets/images/account/icon-withdraw.png" />
-                    <p class="pl-1 text-[#ffffff]">存款教程</p>
-                </div>
-                <div class="flex items-center ml-[40px] text-[#ffffff]">
-                    <p class="text-gray-300">存款极速到账</p>
-                </div>
-            </div>
-            <div class="flex">
-                <div v-for="(item, index) in itemList" :key="index" class="bg-gray-100 pt-2">
-                    <p class="text-center text-[16px]">步骤{{ index + 1 }}/{{ itemList.length }}</p>
-                    <div class=" w-full flex justify-center px-4">
-                        <img class="" :src="item.img" />
-                    </div>
-                </div>
-            </div>
+    <div class="pa-[20px]" style="padding: 20px;">
+        <div class="text-[20px] text-[#ffffff] font-bold ml-[10px]">存款帮助</div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            1.线上支付(已开通网上银行客户)
+        </div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            网上银行在線支付、0手續費、开户5钟完成（支持以下银行）<br />
+            工商银行.招商银行.建设银行.农业银行.中国银行.交通银行.华夏银行.兴业银行.民生银行.<br />
+            广发银行.深圳银行.浦东银行.中信银行.光大银行.重庆银行.南京银行.勃海银行.东亚银行.<br />
+        </div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            2.公司入款(ATM柜员机或银行柜台转账客户)
+        </div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            您需要向在线客服索取我公司银行账户，并通过银行柜台或者ATM柜员机转账至我们公司指定的银行账号。为了保障您的资金安全，请您汇款/转账成功后，及时联系在线客服提供您的存款信息，由在线客服为您确认款项。<br/>
+            【存款注意事项】<br/>
+            1.皇冠现金网城最低存款为$100人民币，最高存款暂不限制。未开通网银的会员，请亲洽您的银行柜台办理。如有任何问题，请联系24小时在线客服，谢谢。 2.皇冠现金网的新老会员通过网银、ATM、手机银行转账或现金到柜台存入到公司指定银行账户进行游戏充值，即可享受充值金额2％的返利。无上限，次次存次次送。
         </div>
     </div>
 </template>
--- src/views/guid_help/index.vue
+++ src/views/guid_help/index.vue
@@ -1,202 +1,34 @@
-<script setup>
-import { ref } from 'vue';
-
-const itemList = ref([
-    {
-        img: new URL('@/assets/images/account/deposit-1.png', import.meta.url).href,
-    },
-    {
-        img: new URL('@/assets/images/account/deposit-2.png', import.meta.url).href,
-    },
-    {
-        img: new URL('@/assets/images/account/deposit-3.png', import.meta.url).href,
-    },
-    {
-        img: new URL('@/assets/images/account/deposit-4.png', import.meta.url).href,
-    }
-]);
-</script>
+<script setup></script>
 
 <template>
-    <div class="animated fadeInLeft mb-[20px] pb-[20px] text-[#ffffff] text-[16px]" style="color: white !important">
-        <div class=" pt-[50px] pb-[60px]  pb-[20px]">
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">取款教程</p>
-                    </div>
-                    <div class="flex items-center">
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex pl-2">
-                    <div class="text-start w-[400px] pt-2">
-                        <span>
-                            1.第一次取款请先绑定银行卡，<br />绑定银行卡步骤：<br /><br />A.登录开云账号&nbsp;--&nbsp;我的页面&nbsp;--&nbsp;<br />账户管理&nbsp;--&nbsp;添加银行卡；<br /><br />B.登录开云账号&nbsp;--&nbsp;首页/我的钱<br />包&nbsp;--&nbsp;点击取款&nbsp;--&nbsp;添加银行卡。<br /><br />2.绑定完成后在取款金额输入<br />框输入金额，点击立即取款。
-                        </span>
-                    </div>
-                    <div class="w-full flex">
-                        <img class="" src="@/assets/images/account/help-content.png" alt="arrow" />
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>可以使用别人的银行卡进行取款吗？</div>
-            <div>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        为了确保客户的账户资金安全，取款需要使用账号本人的 银行卡才可以进行取款哦，且取款银行卡姓名必须与注册 姓名一致。
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">申请取款需要注意些什么？</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        1&nbsp;绑定的银行卡姓名需与开云账户的注册姓名一致。
-                        <br />2&nbsp;绑定的银行卡信息需要正确。
-                        <br />3&nbsp;若您未申请任何红利优惠，投注满一倍流水即可申请取<br />款。
-                        <br />4&nbsp;若申请首存红利，则需要满足优惠活动写明的有效投注<br />额要求。
-                        <br />5&nbsp;全天24小时都可进行取款申请。
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">游戏账户里有钱为什么无法取款？</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        您需要先将资金从游戏平台钱包转至中心钱包后，才能进 行取款操作。
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">可以绑定多张银行卡吗？</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        开云用户可以绑定10张银行卡进行取款操作，在我的页面 选择“账户管理”，按步骤添加银行卡即可。
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">取款为什么还需要审核？</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        取款审核是相关部门在给您办理出款之前的一个简单的核 实步骤，是为了确保客户资金安全，所以需要核实相关信 息。
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">取款要求（例：流水）</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        开云用户只要达到存款全额投注，可以随时申请取款。比 如，您存100元，在该笔存款后，您累计下注达到100元 的取款流水，即可办
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">取款到账时间</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        开云用户取款一般10分钟内到账，如果30分钟还未到账可 以联系在线客服为您核查！
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">取款支持的银行</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        目前为您提供15家取款银行：中国银行、工商银行、建设 银行、交通银行、农业银行、招商银行、民生银行、平安 银行、华夏银行、广东
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">我流水还差多少？</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        首先确认自己是否有申请优惠活动，找到对应优惠活动查 看所需流水，再点击APP右下角【我的】一【投注记录】 查看流水，无申请优惠
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">取款输入金额无法提交申请？</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        1 金额是否从场馆成功转出至中心钱包。
-                        <br />2 若提示”您有订单尚未核实成功”请联系客服进行咨询。 若有其他取款问题请与7*24小
-                    </div>
-                </div>
-            </div>
-            <p class="w-full h-[0.5px] bg-[#CBCBCB]"></p>
-            <div>
-                <div class="flex justify-between text-[15px] items-center py-2  px-2">
-                    <div class="flex">
-                        <p class="">我取款未到账？</p>
-                    </div>
-                </div>
-                <p class="w-full h-[0.5px] bg-[#CBCBCB] ml-2 "></p>
-                <div class="pt-2 flex justify-between px-2 pb-2">
-                    <div class="text-start ">
-                        <span class="paragraph_12">正常情况下10分钟以内即可到账，延迟原因一般是网络波
-                            <br />动、风控审核等，耐心等待片刻即可，若30分钟以后未到
-                            <br />账:联系7*24小时在线客服同时提供以下信息。
-                            <br />（1）会员账号<br />（2）出款时间<br />（3）出款金额</span>
-                    </div>
-                </div>
-            </div>
+    <div class="pa-[20px]" style="padding: 20px;">
+        <div class="text-[20px] text-[#ffffff] font-bold ml-[10px]">取款帮助</div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            【提款注意事项】
+        </div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            1.每笔取款下限为100元人民币，上限为1000000元人民币。到帐时间：一般3-10分钟内划出。<br />
+            2.每个账户24小时内只可以申请3次有效提款，再次提款请于距上次成功提款24小时后申请。<br />
+            3.银行帐号持有人姓名必须与在皇冠现金网注册的姓名一致，否则无法申请提款。<br />
+            4.存款后必须经过全额投注，否则无法申请提款，例如您存款1000元，您的有效投注额需达到1000元方可以申请提款。<br />
+            5.为保障客户资金安全，皇冠现金网有权要求会员提供资料验证，以确保客户资金不会被冒领。<br />
+        </div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            您可以通过以下方式进行提款：<br />
+            第一步：会员登入后点选「在线提款」。<br />
+            第二步：确认提款人姓名与您银行账号持有人相符。<br />
+            第三步：输入「取款额度」以及「有效的手机号码」。如有任何问题，方便皇冠现金网客服人员在第一时间与您联系。<br />
+            第四步：确认提款银行账号正确。<br />
+            第五步：选择公司金流方式出款。绑定建设银行、工商银行、农业银行、招商银行、中国银行、交通银行、中信银行、光大银行、华夏银行、民生总行银行、广东发展银行、深圳发展银行、中国邮政、农村信用合作社为出款银行的会员，随时提款，实时到帐。
+        </div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            皇冠现金网相关优惠或赠送彩金，请详见‘优惠活动’。
+        </div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            注意:账户存在异常操作、违规下注、及其他不符合公司规定的注单；需暂停提款、审核账户；情节严重的，冻结账户，没收余额！
+        </div>
+        <div class="text-[14px] text-[#ffffff] ml-[10px] mt-[20px]">
+            工商银行 农业银行 交通银行 建设银行 招商银行 中国银行
         </div>
     </div>
 </template>
